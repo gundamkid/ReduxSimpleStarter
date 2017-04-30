f4f4d801ae9cc786719ccf23705421a9bd2af30d
Anh Tuan Ngo <ngoanhtuan245@gmail.com>
2017-04-30 23:24:32 +0700

    add search bar to page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyALP5xtDcI1ovwmVJvNz7FonM7G-hNU42c';
 
 const App = () => {
-  return <div>Hello world!</div>  ;
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
