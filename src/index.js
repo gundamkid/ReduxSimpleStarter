b1cf1d24c1a33fa6be535810566b308adfa3f1d6
Anh Tuan Ngo <ngoanhtuan245@gmail.com>
2017-05-04 11:49:42 +0700

    Response and refactor class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyALP5xtDcI1ovwmVJvNz7FonM7G-hNU42c';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: "surfboard"}, (videos) => {
+      this.setState({videos});
+      // is mean this.setState({videos: videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList videos = {this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
